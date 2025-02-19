8f2c3a12b3d3eacd9b37827f723e36e388d87e7e
Manmad <philmade6@gmail.com>
2025-02-19 17:48:59 +0300

    validation development

--- SPL/Lab code/lab_15(3sem)/script.js
+++ SPL/Lab code/lab_15(3sem)/script.js
@@ -1,1 +1,17 @@
-//validation
+//basic checks
+let form = document.forms.inf;
+let surename = form.elements.surename;
+let name = form.elements.name;
+let mail = form.elements.mail;
+let phone = form.elements.phone;
+let city = form.elements.cities;
+let selected = city.value;
+let submit_button = document.getElementById('btn');
+submit_button.addEventListener('click',()=>{
+    if(surename.value = ''){
+        let surename_message = document.getElementById('1er');
+        surename_message.innerHTML = 'This field should not be empty';  
+    }
+    // alert(`${surename.value},${name.value},${selected}`);
+    // city.value = '';
+})
