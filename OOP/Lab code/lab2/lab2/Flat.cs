8102b3caee1fe2d63e006f2492f3a9b53d94aeca
Manmad <philmade6@gmail.com>
2025-02-26 13:30:53 +0300

    Some minor changes

---
+++ OOP/Lab code/lab2/lab2/Flat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Flat
+    {
+        private double _square;
+
+    }
+}
