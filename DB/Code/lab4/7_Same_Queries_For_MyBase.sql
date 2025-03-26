fa9b62fb41d13ebd4757c424db94832cfbeb3c94
Manmad <philmade6@gmail.com>
2025-03-26 12:32:09 +0300

    DB:lab4 queries for myBase added

--- DB/Code/lab4/7_Same_Queries_For_MyBase.sql
+++ DB/Code/lab4/7_Same_Queries_For_MyBase.sql
@@ -1,2 +1,13 @@
 use FIL_MyBase;
 
+select Client.First_name as Name, Client.Last_name as Surename, Orders.Ordered_quantity as Quantity , Products.Product_name as Product from
+Client inner join Orders on Client.Client_id=Orders.Client_id inner join Products on Orders.Product_id=Products.Product_id
+
+
+
+select Client.First_name as Name, Client.Last_name as Surename, Orders.Ordered_quantity as Quantity, Orders.Sale_date as Date, Products.Product_name as Product
+from Client inner join Orders on Client.Client_id=Orders.Client_id inner join Products on Orders.Product_id=Products.Product_id where Client.Has_Discount=1
+order by Date desc
+
+
+
