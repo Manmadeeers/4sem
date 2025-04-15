4b17643f27ab448686c89312e34ed79d95407d1b
Manmad <philmade6@gmail.com>
2025-04-15 19:06:25 +0300

    OOP:lab4-5 in progress. materialdesign connection in progress

--- OOP/Lab code/lab4-5/lab4-5/obj/Debug/net8.0-windows/lab4-5.AssemblyInfo.cs
+++ OOP/Lab code/lab4-5/lab4-5/obj/Debug/net8.0-windows/lab4-5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4-5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12fdaee43bccb416eba5da056c62cf258a595fde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77858ff512a57250e0a61f1fb45a0d4432bfec6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4-5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab4-5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
