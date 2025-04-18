f99c18660e54811cd8d6e13768f4649b0744ea79
Manmad <philmade6@gmail.com>
2025-04-18 13:14:43 +0300

    IPT:lab6 started

---
+++ IPT/Lab code/lab_6/ASPA_006/DAL_Celebrity/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace DAL_Celebrity
+{
+    public class Celebrity
+    {
+        public Celebrity() { this.FullName = string.Empty;this.Nationality = string.Empty; }
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Nationality { get; set; }
+
+        public string? ReqPhotoPath { get; set; }
+
+        public virtual bool Update(Celebrity celebrity);
+    }
+
+    public class LifeEvent
+    {
+        public LifeEvent() { }
+
+        public int Id { get; set; }
+        public int CelebrityId { get; set;}
+        public DateTime Date { get; set;}
+
+        public string Description { get; set; }
+
+        public string? ReqPhotoPath { get; set; }
+
+        public virtual bool Update(LifeEvent lifeEvent);
+    }
+
+}
