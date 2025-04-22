d3cc394326d1bd2a95ca10e6f5b0cedbeb8c4994
Manmad <philmade6@gmail.com>
2025-04-22 16:43:09 +0300

    DB:lab8 done

--- DB/Code/lab8/8_same_queries_for_myBase.sql
+++ DB/Code/lab8/8_same_queries_for_myBase.sql
@@ -47,4 +47,32 @@ from
 go*/
 select * from [Orders view]
 
-/*4 */
+/*4 Client view */
+
+/*go
+create view [Client view]
+as 
+select
+	Client.Client_id as Id,
+	Client.First_name as Name,
+	Client.Last_name as Surename,
+	Client.Email as Email
+from
+	Client
+go*/
+select * from [Client view]
+
+/*5 altered Client view*/
+
+go
+alter view [Client view]
+with schemabinding
+as
+select
+	C.Client_id as Id,
+	C.First_name as Name,
+	C.Last_name as Surename,
+	C.Phone as [Phone number]
+from dbo.Client C
+go
+select * from [Client view]
