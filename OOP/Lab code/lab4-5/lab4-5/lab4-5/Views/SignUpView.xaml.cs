96d8af1d9719d71839adc131a2d416a030ef6915
Manmad <philmade6@gmail.com>
2025-04-22 16:34:05 +0300

    OOP:lab4-5 rework started

---
+++ OOP/Lab code/lab4-5/lab4-5/lab4-5/Views/SignUpView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4_5.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpView.xaml
+    /// </summary>
+    public partial class SignUpView : UserControl
+    {
+        public SignUpView()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Add signup logic here
+            string username = ((TextBox)this.FindName("UsernameTextBox")).Text;
+            string email = ((TextBox)this.FindName("EmailTextBox")).Text;
+            string password = ((PasswordBox)this.FindName("PasswordBox")).Password;
+
+            // Implement validation and registration logic
+            MessageBox.Show($"Registration attempted:\nUsername: {username}\nEmail: {email}");
+        }
+    }
+}
