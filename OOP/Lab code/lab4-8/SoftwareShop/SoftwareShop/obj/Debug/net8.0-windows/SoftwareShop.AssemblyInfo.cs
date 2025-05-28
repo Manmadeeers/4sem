7e687d01b46a2c98121fcf78e609e50b4619bba5
Manmad <philmade6@gmail.com>
2025-05-28 15:59:01 +0300

    OOP:lab4-8 rework in progress

--- OOP/Lab code/lab4-8/SoftwareShop/SoftwareShop/obj/Debug/net8.0-windows/SoftwareShop.AssemblyInfo.cs
+++ OOP/Lab code/lab4-8/SoftwareShop/SoftwareShop/obj/Debug/net8.0-windows/SoftwareShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftwareShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03202d9bf3d6d0f561df976126644c6126795ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be2a64f77f4cb88366319eb1c37d8efbe510ed15")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftwareShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftwareShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
