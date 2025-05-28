a2d9c10994bf3c8fc9a1d7aeaf6caea19384c0cd
Manmad <philmade6@gmail.com>
2025-05-28 13:09:36 +0300

    IPT:lab7 and lab8 archives added

--- IPT/Lab code/lab_7/ASPA/ASPA007_1/obj/Debug/net8.0/ASPA007_1.AssemblyInfo.cs
+++ IPT/Lab code/lab_7/ASPA/ASPA007_1/obj/Debug/net8.0/ASPA007_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPA007_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c91f153b18039f11fc86e49d192f393f9cca8f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+261991b6d47b1399aa235f3743acbb06c268c59a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPA007_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPA007_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
